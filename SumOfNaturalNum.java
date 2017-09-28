b99beefb99a5b689a54633a015011ee31fe6c308
Sridharrr <sridharnarayananit@gmail.com>
2017-09-28 20:15:36 +0530

    Create SumOfNaturalNum.java

---
+++ SumOfNaturalNum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class SumOfNaturalNum {
+	public static void main (String[] args) {
+        Scanner sr=new Scanner(System.in);
+        int n=sr.nextInt();
+        int sum=(n*(n+1))/2;
+        System.out.println(sum);
+	}
+}
